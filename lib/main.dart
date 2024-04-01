27acabaeaad863181b12ca8f30cf7df3b0c39a17
Anmol <39460326+anmol-me@users.noreply.github.com>
2024-04-01 16:26:19 +0530

    Add firebase backend

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:desktop_window/desktop_window.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:universal_platform/universal_platform.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import '/src/shared/app.dart';
 
@@ -10,12 +12,16 @@ Future setDesktopWindow() async {
   await DesktopWindow.setWindowSize(const Size(1300, 900));
 }
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   if (UniversalPlatform.isDesktop) {
     setDesktopWindow();
   }
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(
     const ProviderScope(
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,6 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_window
   dynamic_color
+  firebase_core
   url_launcher_windows
 )
 
